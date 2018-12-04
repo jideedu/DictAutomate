1b7bfaf4cde2517d85051ee379f2eca99530a765
jideedu <jideedu@users.noreply.github.com>
2018-12-04 11:20:38 +0000

    Update DictAutomate.py

--- DictAutomate.py
+++ DictAutomate.py
@@ -41,9 +41,6 @@ for x in range(2):
             # we have to wait for the page to refresh, the last thing that seems to be updated is the title
             #WebDriverWait(driver, 10).until(EC.title_contains(x))
 
-            # You should see the world"
-            #print (driver.title)
-            
             #needed for firefox driver
             time.sleep(3)
             #finally:
